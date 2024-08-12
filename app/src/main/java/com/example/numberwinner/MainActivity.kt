ef72bf4787fc754fcbadf443cafda8c110dcb44c
Hari sudhan <113774103+itsharisudhan@users.noreply.github.com>
2024-08-12 21:57:47 +0530

    modified the code, now it will change color when button is created

--- app/src/main/java/com/example/numberwinner/MainActivity.kt
+++ app/src/main/java/com/example/numberwinner/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.numberwinner
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +16,23 @@ class MainActivity : AppCompatActivity() {
 
         val btnleft = findViewById<Button>(R.id.btnleft)
         val btnright = findViewById<Button>(R.id.btnright)
-        val main = findViewById<Button>(R.id.main)
+        val main = findViewById<ConstraintLayout>(R.id.main)
         btnleft.setOnClickListener{
 
             val leftnum = btnleft.text.toString().toInt()
             val rightnum = btnright.text.toString().toInt()
 
             if (leftnum>rightnum) {
+                main.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this,"answer is correct" , Toast.LENGTH_SHORT).show()
 
             }
+            else {
+                main.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "answer is wrong", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
 
 
