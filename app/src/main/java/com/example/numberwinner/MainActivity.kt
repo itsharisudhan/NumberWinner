e8eea2ae2899cca6245dc463abfdb8d54584e77e
Hari sudhan <113774103+itsharisudhan@users.noreply.github.com>
2024-08-15 00:55:51 +0530

    modified the code, now it will generate random numbers and added functionality to both the buttons

--- app/src/main/java/com/example/numberwinner/MainActivity.kt
+++ app/src/main/java/com/example/numberwinner/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,31 +17,53 @@ class MainActivity : AppCompatActivity() {
         val btnleft = findViewById<Button>(R.id.btnleft)
         val btnright = findViewById<Button>(R.id.btnright)
         val main = findViewById<ConstraintLayout>(R.id.main)
-        btnleft.setOnClickListener{
+        fun newfunc() {
+            val leftnum = Random.nextInt(10)
+            var rightnum = Random.nextInt(10)
+            while (rightnum == leftnum) {
+                rightnum = Random.nextInt(10)
+            }
+            btnleft.text = leftnum.toString()
+            btnright.text = rightnum.toString()
+        }
+
+
+        btnleft.setOnClickListener {
+
+                val leftnum = btnleft.text.toString().toInt()
+                val rightnum = btnright.text.toString().toInt()
+
+                if (leftnum > rightnum) {
+                    main.setBackgroundColor(Color.GREEN)
+                    Toast.makeText(this, "answer is correct", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    main.setBackgroundColor(Color.RED)
+                    Toast.makeText(this, "answer is wrong", Toast.LENGTH_SHORT).show()
+                }
+                newfunc()
+
+            }
+        btnright.setOnClickListener {
 
             val leftnum = btnleft.text.toString().toInt()
             val rightnum = btnright.text.toString().toInt()
 
-            if (leftnum>rightnum) {
+            if (leftnum < rightnum) {
                 main.setBackgroundColor(Color.GREEN)
-                Toast.makeText(this,"answer is correct" , Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "answer is correct", Toast.LENGTH_SHORT).show()
 
-            }
-            else {
+            } else {
                 main.setBackgroundColor(Color.RED)
                 Toast.makeText(this, "answer is wrong", Toast.LENGTH_SHORT).show()
             }
-
+            newfunc()
 
         }
 
-
-
-
-
-
-
-
+        }
 
     }
-}
+
+
+
